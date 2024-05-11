47884448fcb138ee62c647e029f8aac492131d63
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-05-11 18:20:46 +0900

    [Add] prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,21 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ['prettier-plugin-astro'],
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  proseWrap: 'always',
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
