f29038d978a2f7c9e5f8bbc766ae5d79f5c057f6
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-05-25 14:37:26 +0900

    [Update] header z-index

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import NavigationSp from '@/components/NavigationSp';
 
 const Header = () => {
   return (
-    <header className="fixed w-full bg-primary">
+    <header className="fixed w-full bg-primary z-50">
       <div className="container flex justify-between items-center py-2">
         <a href="/" className="text-white font-medium text-xl md:text-2xl">
           Rowicy
