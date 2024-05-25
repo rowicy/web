82d42ea6364a723d4f5ff18a295c3613f3485070
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-05-25 14:17:40 +0900

    [Update] config format

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import partytown from '@astrojs/partytown';
 export default defineConfig({
   site: 'https://www.rowicy.com',
   prefetch: {
-    prefetchAll: true
+    prefetchAll: true,
   },
   integrations: [
     react(),
