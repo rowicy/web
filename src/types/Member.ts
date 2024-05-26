d856af1d2982b7a1cdb74df090e5199f503eba51
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-05-26 17:50:18 +0900

    [Add] Icon

--- src/types/Member.ts
+++ src/types/Member.ts
@@ -2,7 +2,7 @@ type Member = {
   name: string;
   image: string;
   description?: string;
-  links?: { name: string; id?: string; href: string }[];
+  links?: { name: string; id?: string; href: string; type?: 'icon' }[];
 };
 
 export type { Member };
