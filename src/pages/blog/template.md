e4d7685ab138e598e0dbb48995d11eac82186a57
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-06-01 01:39:27 +0900

    [Add] Blog詳細 固定部分作成

---
+++ src/pages/blog/template.md
@@ -0,0 +1,8 @@
+---
+layout: ../../layouts/BlogLayout.astro
+title: 'テンプレート'
+date: 2024-06-01
+description: 'テンプレートです。'
+author: 'Naoki'
+tags: [Techm, 2]
+---
---
+++ src/types/Frontmatter.ts
@@ -0,0 +1,10 @@
+type Frontmatter = {
+  layout: string;
+  title: string;
+  date: string;
+  description: string;
+  author: string;
+  tags: string[];
+};
+
+export type { Frontmatter };
