c6a70b5074798cd3ba9e260d92dd1ad8478506d1
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-06-05 00:05:31 +0900

    [Add] ButtonLink rel指定

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -4,11 +4,12 @@ type Props = {
   href: string;
   icon?: boolean;
   blank?: boolean;
+  rel?: string;
   label?: string;
   children?: React.ReactNode;
 };
 
-const ButtonLink = ({ href, icon, blank, label, children }: Props) => {
+const ButtonLink = ({ href, icon, blank, rel, label, children }: Props) => {
   return (
     <div className="flex justify-center mt-6">
       <Button
@@ -17,7 +18,12 @@ const ButtonLink = ({ href, icon, blank, label, children }: Props) => {
         size={icon ? 'icon' : undefined}
         asChild
       >
-        <a href={href} target={blank ? '_blank' : undefined} aria-label={label}>
+        <a
+          href={href}
+          target={blank ? '_blank' : undefined}
+          rel={rel}
+          aria-label={label}
+        >
           {children}
         </a>
       </Button>
