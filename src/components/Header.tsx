d8bbf38a62d97c69db0463984604a622bec3f096
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-06-08 03:02:00 +0900

    [Update] 変数指定できそうな箇所を置換

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import NavigationPc from '@/components/NavigationPc';
 import NavigationSp from '@/components/NavigationSp';
+import siteInfo from '@/data/siteInfo';
 
 const Header = () => {
   return (
     <header className="fixed w-full bg-primary z-50">
       <div className="container flex justify-between items-center py-2">
         <a href="/" className="text-white font-medium text-xl md:text-2xl">
-          Rowicy
+          {siteInfo.appName}
         </a>
 
         <NavigationPc />
