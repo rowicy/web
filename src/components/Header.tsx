9c24de6a935eb13c786e001cc060ddb990e158ff
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-06-10 23:33:40 +0900

    [Add] header social link設置

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,14 @@ import NavigationPc from '@/components/NavigationPc';
 import NavigationSp from '@/components/NavigationSp';
 import siteInfo from '@/data/siteInfo';
 
-const Header = () => {
+type Props = {
+  children: React.ReactNode;
+};
+
+const Header = ({ children }: Props) => {
   return (
     <header className="fixed w-full bg-primary z-50">
-      <div className="container flex justify-between items-center py-2">
+      <div className="container flex justify-between items-center py-2 gap-6">
         <a
           href="/"
           className="text-white font-medium text-xl md:text-2xl transition hover:opacity-70"
@@ -14,6 +18,8 @@ const Header = () => {
         </a>
 
         <NavigationPc />
+
+        <div className="ml-auto md:ml-0">{children}</div>
 
         <NavigationSp />
       </div>
