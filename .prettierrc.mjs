007a4f76866ae97d4219076a0b2dee56d7f6d40b
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2025-05-24 14:55:19 +0900

    🐳 chore: prettier-plugin-tailwindcss

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,6 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ['prettier-plugin-astro'],
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
   printWidth: 80,
   tabWidth: 2,
   useTabs: false,
