73c365875eadb22075706d81a400845809bfb141
riiim400th <riiim@rowicy.com>
2026-01-22 10:27:17 +0900

    🐞 fix: site_url

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,5 +1,1 @@
-export const SITE_URL = process.env.VERCEL_ENV === 'production' 
-    ? 'https://preview-rowicy-web-riiim.riiimparm.com' // 本番ドメインが決まっているならここ
-    : process.env.VERCEL_URL 
-      ? `https://${process.env.VERCEL_URL}` // プレビューURLなど
-      : 'http://localhost:4321'         // ローカル開発用
+export const SITE_URL = 'web-riiim400ths-projects.vercel.app'
